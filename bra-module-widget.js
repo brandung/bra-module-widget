0a0c0be4801abfde003bbe0a35a1f3d29dc2b5b4
Simon Kemmerling <Simpsonight@users.noreply.github.com>
2015-11-13 11:16:07 +0100

    Update to new WF

--- bra-module-widget.js
+++ bra-module-widget.js
@@ -1,5 +1,5 @@
 /**
- * bra_module-widget.js v1.0
+ * bra_module-widget.js v1.1.0
  * https://github.com/brandung/bra-module-widget
  *
  * Insert widget in _modules.html
@@ -24,7 +24,7 @@
 				widgetName: 'bra-module-widget',		// String: Selector name of the widget
 				mwHeader: '.mw-header',					// Selector: Widget Header
 				mwContainer: '.mw-container',			// Selector: Widget Container
-				deepLinkObj: '.mod-headline',			// Selector: Selector to navigate
+				deepLinkObj: '.mw-headline',			// Selector: Selector to navigate
 				isStickyHeader: true,					// Boolean: set sticky header value
 				stickyHeader: '.main-nav-wrapper',		// Selector: Sticky Header wrapper
 
@@ -62,16 +62,25 @@
 			// DOM of widget
 			settings.widget = $('<div id="' + settings.widgetName + '">' +
 				'<div class="mw-header">' +
-				'	<h3>Modules-Widget</h3><span class="mw-open">Open</span>' +
+				'	<h3>Modules overview</h3><span class="mw-remove" title="Remove">Remove</span><span class="mw-grid" title="Show grid">Show grid</span><span class="mw-open" title="Show module list">Open</span>' +
 				'</div>' +
 				'<div class="mw-container">' +
 				'</div>' +
 				'</div>');
 
 			// bind click event
-			settings.widget.find('span').on('click', function () {
+			settings.widget.find('.mw-open').on('click', function () {
 				$(this).toggleClass('is-active');
 				methods._showHide(settings);
+			});
+
+			settings.widget.find('.mw-grid').on('click', function () {
+				$(this).toggleClass('is-active');
+				methods._toggleGrid();
+			});
+
+			settings.widget.find('.mw-remove').on('click', function () {
+				settings.widget.remove();
 			});
 
 			// append widget
@@ -83,6 +92,10 @@
 			// bind draggable event
 			methods._draggable.call(settings);
 
+		},
+
+		_toggleGrid: function () {
+			$('body').toggleClass('util-show-grid');
 		},
 
 		/**
@@ -109,12 +122,21 @@
 				links = $('<ul></ul>');
 
 			$(settings.deepLinkObj).each(function () {
-				$('<li>' + $(this).text() + '</li>').appendTo(links);
+				var text = $(this).text();
+
+				if(!/^\d+\.\W/ig.test(text)) {
+					text = '&nbsp;&nbsp;&nbsp;&nbsp;' + text;
+				}
+
+				$('<li>' + text + '</li>').appendTo(links);
 			});
 
 			links.find('li').on('click', function () {
 				var $self = $(this),
+					selfText = $.trim($self.text()),
 					headerHeight = 0;
+
+				console.log(selfText);
 
 				// if sticky header is in use get height of sticky element
 				if (settings.isStickyHeader) {
@@ -124,11 +146,13 @@
 				// get element top position
 				// and scroll to
 				$(settings.deepLinkObj).each(function () {
-					if ($(this).text() === $self.text()) {
+					if ($(this).text() === selfText) {
 						var topPos = $(this).offset().top - headerHeight;
+
 						$('body').stop().animate({'scrollTop': topPos}, 'fast', function () {
 							return;
 						});
+
 						return false;
 					}
 				})
@@ -147,7 +171,7 @@
 			var settings = this;
 
 			$(settings.mwHeader).css('cursor', 'move').on("mousedown", function (e) {
-				var $drag = $(this).addClass('active-handle').parent().addClass('draggable');
+				var $drag = $(this).addClass('active-handle').parent().addClass('mw-draggable');
 
 				var z_idx = $drag.css('z-index'),
 					drg_h = $drag.outerHeight(),
@@ -156,18 +180,18 @@
 					pos_x = $drag.offset().left + drg_w - e.pageX;
 
 				$drag.css('z-index', 1000).parents().on("mousemove", function (e) {
-					$('.draggable').offset({
+					$('.mw-draggable').offset({
 						top: e.pageY + pos_y - drg_h,
 						left: e.pageX + pos_x - drg_w
 					}).on("mouseup", function () {
-						$(this).removeClass('draggable').css('z-index', z_idx);
+						$(this).removeClass('mw-draggable').css('z-index', z_idx);
 					});
 				});
 
 				e.preventDefault();
 
 			}).on("mouseup", function () {
-				$(this).removeClass('active-handle').parent().removeClass('draggable');
+				$(this).removeClass('active-handle').parent().removeClass('mw-draggable');
 			});
 		}
 
